56c9585aca0997f87d4a0aaefac73ceae08e5d0d
Chris Abrams <mail@chrisabrams.com>
2012-06-24 13:10:01 -0700

    Factorin

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ useragent.js
 ============
 
 Get the current user agent using Javascript.
+
+##Build
+Build requires uglify-js which is a node.js module
+
+	npm install
+	./build
---
+++ src/Get.js
@@ -0,0 +1,38 @@
+userAgent.get = function(callback) {
+	var userAgent = {};
+
+	switch(navigator.vendor) {
+		case 'Apple Computer, Inc.':
+			if(navigator.userAgent.indexOf('iPod') != -1) {
+				userAgent.device = 'iPod';
+			}
+
+			if(navigator.userAgent.indexOf('iPad') != -1) {
+				userAgent.device = 'iPad';
+			}
+
+			if(navigator.userAgent.indexOf('iPhone') != -1) {
+				userAgent.device = 'iPhone';
+			}
+
+			if(!userAgent.device) {
+				userAgent.device = 'Desktop';
+			}
+
+			break;
+
+		case 'Google Inc.':
+			//Will do Chromebooks later
+			break;
+
+		default:
+			userAgent.device = 'Desktop';
+	}
+
+	if(typeof callback === "function") {
+		callback();
+		return;
+	}
+	
+	return userAgent;
+};
